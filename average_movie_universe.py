66b0eebfe3b627951112c279263131e7cacdad53
David Starner <prosfilaes@gmail.com>
2012-11-17 08:42:25 -0800

    new average_movie_universe to kill much duplicated code

--- average_movie_universe.py
+++ average_movie_universe.py
@@ -30,31 +30,134 @@ def average_bacon_num (movie_tree, total_number_of_movies):
         total_bacon += j * len(movie_tree [j])
     return total_bacon / total_number_of_movies 
 
+def create_table_m2m (table_num):
+    cur.execute ("CREATE TEMPORARY TABLE m2m "
+                 "(movie_id1 SMALLINT (5) UNSIGNED NOT NULL, "
+                 "movie_id2 SMALLINT (5) UNSIGNED NOT NULL, "
+                 "INDEX mid_idx (movie_id1));")
+    if table_num == 1:
+        cur.execute ("INSERT INTO m2m "
+                     "SELECT DISTINCT p1.movie_id, p2.movie_id FROM actor p1 "
+                     "INNER JOIN actor p2 ON p1.person = p2.person AND p1.movie_id != p2.movie_id;")
+    elif table_num == 2 or table_num == 5:
+        cur.execute ("CREATE TEMPORARY TABLE small_dvd_movies "
+                     "(movie_id SMALLINT (5) UNSIGNED UNIQUE NOT NULL, "
+                     "INDEX mid_idx (movie_id)); ")
+        if table_num == 2:
+            cur.execute ('INSERT INTO small_dvd_movies '
+                         'SELECT DISTINCT movie_id FROM dvd d '
+                         'INNER JOIN dvd_contents dc ON dc.dvd_id = d.dvd_id '
+                         'WHERE d.dvd_id NOT IN '
+                         '(SELECT dvd_id FROM dvd_tags WHERE tag = "large movie pack");');
+            cur.execute ('INSERT IGNORE INTO small_dvd_movies '
+                         'SELECT movie_id FROM movie WHERE have_watched;')
+        elif table_num == 5:
+            #The full length only version of the above
+            cur.execute ('INSERT INTO small_dvd_movies '
+                         'SELECT DISTINCT movie_id FROM dvd d '
+                         'INNER JOIN dvd_contents dc ON dc.dvd_id = d.dvd_id '
+                         'INNER JOIN movie m ON dc.movie_id = m.movie_id AND m.is_full_length '
+                         'WHERE d.dvd_id NOT IN '
+                         '(SELECT dvd_id FROM dvd_tags WHERE tag = "large movie pack");');
+            cur.execute ('INSERT IGNORE INTO small_dvd_movies '
+                         'SELECT movie_id FROM movie WHERE have_watched and is_full_length;')
+        else:
+            assert True, "This should be unreachable"
+        cur.execute ("INSERT INTO m2m "
+                     "SELECT DISTINCT p1.movie_id, p2.movie_id FROM actor p1 "
+                     "INNER JOIN actor p2 ON p1.person = p2.person AND p1.movie_id != p2.movie_id "
+                     "WHERE p1.movie_id IN (SELECT movie_id from small_dvd_movies); ")  
+        #The natural formulation doesn't work in MySQL, since you can't reference the temporary table small_dvd_movies
+        #twice in one statement
+        cur.execute ("DELETE FROM m2m WHERE movie_id2 NOT IN (SELECT movie_id from small_dvd_movies);")
+    elif table_num == 3:
+        cur.execute ("INSERT INTO m2m "
+                     "SELECT DISTINCT m1.movie_id, m2.movie_id FROM movie m1 "
+                     "INNER JOIN actor p1 ON p1.movie_id = m1.movie_id "
+                     "INNER JOIN actor p2 ON p1.person = p2.person "
+                     "INNER JOIN movie m2 on p2.movie_id = m2.movie_id AND m2.have_watched "
+                     "WHERE m1.have_watched;")
+    elif table_num == 4:
+        cur.execute ("INSERT INTO m2m "
+                     "SELECT DISTINCT p1.movie_id, p2.movie_id FROM actor p1 "
+                     "INNER JOIN actor p2 "
+                     "ON p1.person = p2.person AND p1.movie_id != p2.movie_id "
+                     "INNER JOIN movie m1 ON p1.movie_id = m1.movie_id AND m1.is_full_length "
+                     "INNER JOIN movie m2 ON p2.movie_id = m2.movie_id AND m2.is_full_length "
+                     ";")
+    elif table_num == 6:
+        cur.execute ("INSERT INTO m2m "
+                     "SELECT DISTINCT m1.movie_id, m2.movie_id FROM movie m1 "
+                     "INNER JOIN actor p1 ON p1.movie_id = m1.movie_id "
+                     "INNER JOIN actor p2 ON p1.person = p2.person "
+                     "INNER JOIN movie m2 ON p2.movie_id = m2.movie_id AND m2.have_watched AND m2.is_full_length "
+                     "WHERE m1.have_watched AND m1.is_full_length;")
+    elif table_num == 7:
+        cur.execute ("INSERT INTO m2m "
+                     "SELECT DISTINCT p1.movie_id, p2.movie_id FROM actor p1 "
+                     "INNER JOIN actor p2 "
+                     "ON p1.person = p2.person AND p1.movie_id != p2.movie_id "
+                     "INNER JOIN movie m1 ON p1.movie_id = m1.movie_id  "
+                     "INNER JOIN movie m2 ON p2.movie_id = m2.movie_id  "
+                     "WHERE m1.movie_id in (SELECT movie_id FROM tv_show) AND "
+                     "m2.movie_id in (SELECT movie_id FROM tv_show) "
+                     ";")
+    elif table_num == 8:
+        cur.execute ("INSERT INTO m2m "
+                     "SELECT DISTINCT m1.movie_id, m2.movie_id FROM movie m1 "
+                     "INNER JOIN actor p1 ON p1.movie_id = m1.movie_id "
+                     "INNER JOIN actor p2 ON p1.person = p2.person "
+                     "INNER JOIN movie m2 ON p2.movie_id = m2.movie_id AND m2.have_watched AND m2.is_full_length "
+                     "INNER JOIN tags t1 ON t1.movie_id = m1.movie_id AND t1.tag = 'science fiction' "
+                     "INNER JOIN tags t2 ON t2.movie_id = m2.movie_id AND t2.tag = 'science fiction' "
+                     "WHERE m1.have_watched AND m1.is_full_length;")
+    else:
+        assert True, "table_nums above 8 are unknown"
+    return
+
 try:
+    MAX_TABLENUM = 8
+    # This is inappropriately intertangled in so many ways.
+    # The appropriate thing would be to find the largest subset
+    # instead of storing a value known to be in the largest subset.
+    # That's more work, both coding and computational, and speed
+    # matters for this code.
+    initial_movie_id = [None, 376, 376, 376, 376, 376, 376, 4584, 51]
+    if (len(sys.argv) != 2 or int(sys.argv[1]) < 1 or
+        int(sys.argv[1]) > MAX_TABLENUM):
+        
+        print ("This program takes one argument, the tablenum to calculate, in the range 1 .. {}."
+               "".format (MAX_TABLENUM))
+        sys.exit()
+        
+    table_num = int(sys.argv[1])
     start_time = time.clock ()
     global con, cur
     insert = True
     con = mdb.connect('localhost', 'dvdeug', '', 'DVDs', use_unicode=True, charset="utf8")
     cur = con.cursor()
     cur.execute ("SET NAMES 'utf8'")
-    cur.execute ("CREATE TEMPORARY TABLE m2m "
-                 "(movie_id1 SMALLINT (5) UNSIGNED NOT NULL, "
-                 "movie_id2 SMALLINT (5) UNSIGNED NOT NULL, "
-                 "INDEX mid_idx (movie_id1));")
-    cur.execute ("INSERT INTO m2m "
-                 "SELECT DISTINCT p1.movie_id, p2.movie_id FROM actor p1 "
-                 "INNER JOIN actor p2 ON p1.person = p2.person AND p1.movie_id != p2.movie_id;")
-    cur.execute ("SELECT movie_id, bacon_num FROM moviebacon WHERE table_num = 1;")
+    create_table_m2m (table_num)
+    cur.execute ("SELECT movie_id, bacon_num FROM moviebacon WHERE table_num = {};"
+                 "".format(table_num))
     prior_bacon_nums = dict (cur.fetchall ())
     setup_end_time = time.clock ()
-    (global_movie_set, movie_tree) = calculate_movie_data (376)
+    root_id = initial_movie_id[table_num]
+    (global_movie_set, movie_tree) = calculate_movie_data (root_id)
     num_movies = len (global_movie_set)
-    bacon_list = [(376, average_bacon_num (movie_tree, num_movies))]
-    global_movie_set.remove (376)
+    cur.execute ("SELECT description FROM moviebacon_tablenum WHERE table_num = {};"
+                 "".format(table_num))
+    print ("The {} list following is about the relations of {} movies."
+           "".format (cur.fetchone()[0], num_movies))
+    bacon_list = [(root_id, average_bacon_num (movie_tree, num_movies))]
+    global_movie_set.remove (root_id)
     zero_count = 0
     for movie in global_movie_set:
         bacon_list.append ((movie, average_bacon_num (calculate_movie_data (movie)[1], num_movies)))
         if zero_count > -1:
+            if bacon_list [-1][0] not in prior_bacon_nums:
+                zero_count = -1
+                continue
             if bacon_list[-1][1] == prior_bacon_nums [bacon_list[-1][0]]:
                 zero_count += 1
             else:
@@ -66,12 +169,12 @@ try:
  
     bacon_list.sort (key=lambda x: x[1])
     if insert:
-        cur.execute ("DELETE FROM moviebacon WHERE table_num = 1;")
+        cur.execute ("DELETE FROM moviebacon WHERE table_num = {};".format (table_num))
     for x in bacon_list:
         cur.execute ("SELECT name, year FROM movie WHERE movie_id = {};".format (x[0]));
         name = cur.fetchall ()
         if insert:
-            cur.execute ("INSERT into moviebacon VALUES (1, {}, {});".format (x[0], x[1]))
+            cur.execute ("INSERT into moviebacon VALUES ({}, {}, {});".format (table_num, x[0], x[1]))
         print ("{} ({}): {:.4f}".format(name[0][0], name[0][1], x[1]), end = ";")
         if x[0] in prior_bacon_nums:
             print (" / old {:.4f} / change {:+.4f}"
--- nightly.sh
+++ nightly.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 q=`mdate | cut -c6-12`
 mysqldump DVDs --user dvdeug --password > dbbackup.$q
-# We've update the actor's table, but not all programs
-#./average_bacon_universe_note_watch.py > averagebaconuniverse.nw.$q
-./average_tv_universe.py > averagetvuniverse.$q
-./average_wfl_universe.py > averagewfluniverse.$q
-./average_watched_universe.py > averagewatcheduniverse.$q
-./average_smalldvd.py > averagesmalluniverse.$q
-./average_fulllength_universe.py > averagefluniverse.$q
-./average_movie_universe.py > averagemovieuniverse.$q
+./average_movie_universe.py 8 > averagewscififl.$q # more of a test
+./average_movie_universe.py 6 > averagewfluniverse.$q
+./average_movie_universe.py 3 > averagewatcheduniverse.$q
+./average_movie_universe.py 5 > averagesmallfl.$q
+./average_movie_universe.py 2 > averagesmalluniverse.$q
+./average_movie_universe.py 4 > averagefluniverse.$q
+./average_movie_universe.py 7 > averagetvuniverse.$q
+./average_movie_universe.py 1 > averagemovieuniverse.$q
+
+#Actors are relatively uninteresting and relatively large, so don't usually run
 # We never look at it, so why burn the energy?
 #./average_bacon_universe_watched.py > averagebaconuniverse.watch.$q 
 #./average_bacon_universe_quick.py > averagebaconuniverse.quick.$q 
 # Will it ever complete?
 #./average_bacon_universe_notable.py > averagebaconuniverse.notable.$q
+#./average_bacon_universe_note_watch.py > averagebaconuniverse.nw.$qY
