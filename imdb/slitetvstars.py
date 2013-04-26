72bf7b6db96e8d7f77405923afe34d5aaa0d3940
David Starner <prosfilaes@gmail.com>
2013-04-26 05:07:33 -0700

    patched; bunch of code has been cut out, but it should handle what it does handle correctly

--- imdb/slitetvstars.py
+++ imdb/slitetvstars.py
@@ -44,11 +44,8 @@ def parse_episode (episode): # return Episode
             return Episode(name="", season_num=episode_match.group(1),
                            episode_num=episode_match.group(2))
         else:
-            episode_match = re.match (r"\(([^)]*)\)", episode)
-            if episode_match:
-                return Episode(name="", season_num=episode_match.group(1), episode_num="")
-            else:
-                return Episode(name=episode, season_num="", episode_num="")
+            # If we lack episode_num or season_num, just raise ValueError
+            raise ValueError
 
 def process_line (line):
 # returns (Full_Movie, person string)
@@ -97,12 +94,7 @@ def process_line (line):
                     movie_match.group (3)))
             return
         if version != "":
-            new_tv_show = tv_show + " (" + version + ")"
-            if tv_show in variants:
-                (variants[tv_show]).add (new_tv_show)
-            else:
-                variants[tv_show] = set([new_tv_show])
-            tv_show = new_tv_show
+           return
         full_movie = Full_Movie(show_name=tv_show, year=year, episode=episode)
     else:
         return
@@ -133,7 +125,7 @@ show_actor = dict ()
 show_with_episodes = dict ()
 show_director = dict ()
 #variants = dict ()
-count = 0
+#count = 0
 with open ("act_list", "r") as f:
     for line in f:
         count = count + 1
@@ -147,11 +139,11 @@ with open ("act_list", "r") as f:
             show_actor[movie].add(actor)
         else:
             show_actor[movie] = set([actor])
-        if count > 10000:
-            break
+#        if count > 10000:
+#            break
 # load in directors
 name = ""
-count = 0
+#count = 0
 with open ("directors.list", "r") as f:
     for line in f:
         count = count + 1
@@ -165,8 +157,8 @@ with open ("directors.list", "r") as f:
             show_director[movie].add(director)
         else:
             show_director[movie] = set([director])
-        if count > 10000:
-            break
+#        if count > 10000:
+#            break
 
 print ("Dumping to SQLite")
 conn = sqlite3.connect('tv_show.db')
@@ -197,9 +189,17 @@ curr.execute ("CREATE TABLE show_no_episode_info (show_name TEXT, year INTEGER, 
 #shutil.copy ('tv_show.db', 'tv_show.db_1')
 
 for show in show_with_episodes:
-    for swe in show_with_episodes [show]:
-        curr.execute ("INSERT INTO episode_names VALUES (?, ?, ?, ?, ?);",
-                      (swe[0], swe[2][1], swe[2][2], swe[2][0], swe[1]))
+    for swe in show_with_episodes [show]: #swe is a Full_Movie
+        curr.execute ("SELECT * FROM episode_names WHERE show_name = ? and season = ? and epsiode = ? and year = ?;",
+                      (swe.show_name, swe.episode.season_num, swe.episode.episode_num, swe.year))
+        matches = curr.fetchall ()
+        if len (matches) > 0:
+            print ("Clashing entries for episode_names!")
+            print (matches)
+            print (swe)
+        else:   
+            curr.execute ("INSERT INTO episode_names VALUES (?, ?, ?, ?, ?);",
+                          (swe.show_name, swe.episode.season_num, swe.episode.episode_num, swe.episode.name, swe.year))
 conn.commit ()
 shutil.copy ('tv_show.db', 'tv_show.db_2')
 
@@ -213,9 +213,6 @@ for show in show_actor: # show is Full_Movie
             # We're not using IGNORE here, in the hopes that we've cleared up the problems with this stuff
             curr.execute ("INSERT INTO episode_actors VALUES (?, ?, ?, ?);",
                           (show.show_name, show.year, show.episode.season_num, show.episode.episode_num, actor))
-        else:
-            print ("ERROR: {} is a malformed key in show_actor.".format (show))
-            sys.exit ()
 conn.commit ()
 shutil.copy ('tv_show.db', 'tv_show.db_3')
 
@@ -228,9 +225,6 @@ for show in show_director:
         else: # has episode information
             curr.execute ("INSERT INTO episode_directors VALUES (?, ?, ?, ?);",
                             (show.show_name, show.year, show.episode.season_num, show.episode.episode_num, director))
-        else:
-            print ("ERROR: {} is a malformed key in show_director.".format (show))
-            sys.exit ()
 conn.commit ()
 shutil.copy ('tv_show.db', 'tv_show.db_4')
 
